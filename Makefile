97fa81914cc2831675898c0e5c958125da8c20b4
eionrobb <eionrobb@21dcaca6-556a-90a6-bc24-973ef114c1da>
2010-09-06 02:23:44 +0000

    Initial checkin

---
+++ Makefile
@@ -0,0 +1,40 @@
+#Customisable stuff here
+LINUX32_COMPILER = i686-pc-linux-gnu-gcc
+LINUX64_COMPILER = x86_64-pc-linux-gnu-gcc
+WIN32_COMPILER = /usr/bin/i586-mingw32-gcc
+WIN32_WINDRES = i586-mingw32-windres
+#LINUX_ARM_COMPILER = arm-pc-linux-gnu-gcc
+LINUX_ARM_COMPILER = arm-none-linux-gnueabi-gcc
+LINUX_PPC_COMPILER = powerpc-unknown-linux-gnu-gcc
+FREEBSD60_COMPILER = i686-pc-freebsd6.0-gcc
+MACPORT_COMPILER = i686-apple-darwin9-gcc-4.0.1
+
+LIBPURPLE_CFLAGS = -I/usr/include/libpurple -I/usr/local/include/libpurple 
+GLIB_CFLAGS = -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/lib/gtk-2.0/include/ -I/usr/include -I/usr/local/include/glib-2.0 -I/usr/local/lib/glib-2.0/include -I/usr/local/include
+WIN32_DEV_DIR = /root/pidgin/win32-dev
+WIN32_PIDGIN_DIR = /root/pidgin/pidgin-2.3.0_win32
+WIN32_CFLAGS = -I${WIN32_DEV_DIR}/gtk_2_0/include/glib-2.0 -I${WIN32_PIDGIN_DIR}/libpurple/win32 -I${WIN32_PIDGIN_DIR}/pidgin/win32 -I${WIN32_DEV_DIR}/gtk_2_0/include -I${WIN32_DEV_DIR}/gtk_2_0/include/glib-2.0 -I${WIN32_DEV_DIR}/gtk_2_0/lib/glib-2.0/include -I${WIN32_DEV_DIR}/gtk_2_0/lib/gtk-2.0/include -Wno-format
+WIN32_LIBS = -L${WIN32_DEV_DIR}/gtk_2_0/lib -L${WIN32_PIDGIN_DIR}/libpurple -L${WIN32_PIDGIN_DIR}/pidgin -lglib-2.0 -lgobject-2.0 -lintl -lpidgin -lpurple -lws2_32 -L. -lgtk-win32-2.0
+MACPORT_CFLAGS = -I/opt/local/include/libpurple -I/opt/local/include/glib-2.0 -I/opt/local/lib/glib-2.0/include -I/opt/local/include -arch i386 -arch ppc -dynamiclib -L/opt/local/lib -lpidgin -lpurple -lglib-2.0 -lgobject-2.0 -lintl -lz -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4
+
+DEB_PACKAGE_DIR = ./debdir
+
+SOURCES = \
+	icon_override.c
+	
+#Standard stuff here
+.PHONY:	all clean install sourcepackage
+
+all:	icon_override.dll icon_override.so
+
+install:
+	cp icon_override.so /usr/lib/purple-2/
+clean:
+	rm -f icon_override.dll icon_override.so
+
+icon_override.so:	${SOURCES}
+	${LINUX32_COMPILER} ${LIBPURPLE_CFLAGS} -Wall ${GLIB_CFLAGS} -I. -g -O2 -pipe ${SOURCES} -o $@ -shared -fPIC -DPIC
+
+icon_override.dll:	${SOURCES}
+	${WIN32_COMPILER} ${LIBPURPLE_CFLAGS} -Wall -I. -g -O0 -pipe ${SOURCES} -o $@ -shared -mno-cygwin ${WIN32_CFLAGS} ${WIN32_LIBS}
+	upx $@
---
+++ icon_override.c
@@ -0,0 +1,162 @@
+/*
+http://eion.robbmob.com/purple-blist-sort.png
+*/
+
+#define PURPLE_PLUGINS
+
+#include <glib.h>
+#include <plugin.h>
+#include <account.h>
+#include <accountopt.h>
+
+#ifndef _
+#	define _(a) a
+#endif
+
+#define NEW_ICON_ID "eionrobb_new_prpl_icon"
+static GHashTable *original_list_icon = NULL;
+
+const char *
+new_list_icon(PurpleAccount *account, PurpleBuddy *buddy)
+{
+	const char*(* list_icon_func)(PurpleAccount *account, PurpleBuddy *buddy);
+	const char *prpl_id;
+	const char *new_icon;
+	PurpleAccount *acct;
+	
+	if (account != NULL)
+		acct = account;
+	else if (buddy != NULL)
+		acct = buddy->account;
+	else
+		return "";
+	
+	new_icon = purple_account_get_string(account, NEW_ICON_ID, NULL);
+	if (new_icon && *new_icon)
+		return new_icon;
+	
+	prpl_id = account->protocol_id;
+	list_icon_func = g_hash_table_lookup(original_list_icon, prpl_id);
+	return list_icon_func(account, buddy);
+}
+
+static gboolean
+plugin_load(PurplePlugin *plugin)
+{
+	GList *proto_list;
+	PurplePlugin *proto;
+	PurpleAccountOption *option;
+	PurplePluginProtocolInfo *prpl_info;
+	
+	//Hook into every protocol's prpl_info->list_icon function
+	original_list_icon = g_hash_table_new(g_str_hash, g_str_equal);
+	
+	proto_list = purple_plugins_get_protocols();
+	
+	for(; proto_list; proto_list = proto_list->next)
+	{
+		proto = proto_list->data;
+		prpl_info = PURPLE_PLUGIN_PROTOCOL_INFO(proto);
+	
+		g_hash_table_insert(original_list_icon, (gpointer)purple_plugin_get_id(proto), prpl_info->list_icon);
+		prpl_info->list_icon = new_list_icon;
+
+		//Add in some protocol preferences
+		option = purple_account_option_string_new(_("Protocol Icon"), NEW_ICON_ID, "");
+		prpl_info->protocol_options = g_list_append(prpl_info->protocol_options, option);
+	}
+	
+	return TRUE;
+}
+
+static gboolean
+plugin_unload(PurplePlugin *plugin)
+{
+	//Unhook
+	GList *proto_list;
+	gpointer func;
+	GList *list, *llist;
+	PurplePluginProtocolInfo *prpl_info;
+	PurpleAccountOption *option;
+	PurplePlugin *proto;
+	
+	proto_list = purple_plugins_get_protocols();
+	for(; proto_list; proto_list = proto_list->next)
+	{
+		proto = (PurplePlugin *) proto_list->data;
+		prpl_info = PURPLE_PLUGIN_PROTOCOL_INFO(proto);
+		func = g_hash_table_lookup(original_list_icon, purple_plugin_get_id(proto));
+		if (func)
+		{
+			prpl_info->list_icon = func;
+			g_hash_table_remove(original_list_icon, purple_plugin_get_id(proto));
+		}
+		
+		//Remove from the protocol options screen
+		list = prpl_info->protocol_options;
+		while(list != NULL)
+		{
+			option = (PurpleAccountOption *) list->data;
+			if (g_str_equal(purple_account_option_get_setting(option), NEW_ICON_ID))
+			{
+				llist = list;
+				if (llist->prev)
+					llist->prev->next = llist->next;
+				if (llist->next)
+					llist->next->prev = llist->prev;
+				
+				purple_account_option_destroy(option);
+				
+				list = g_list_next(list);
+				g_list_free_1(llist);
+			} else {
+				list = g_list_next(list);
+			}
+		}
+	}
+	g_hash_table_destroy(original_list_icon);
+	original_list_icon = NULL;
+	
+	return TRUE;
+}
+
+static void
+plugin_init(PurplePlugin *plugin)
+{
+}
+
+static PurplePluginInfo info = 
+{
+	PURPLE_PLUGIN_MAGIC,
+	2,
+	2,
+	PURPLE_PLUGIN_STANDARD,
+	NULL,
+	0,
+	NULL,
+	PURPLE_PRIORITY_DEFAULT,
+
+	"eionrobb-icon-override",
+	"Protocol Icon Override",
+	"0.1",
+	"Customise protocol icons",
+	"Lets you change protocol icons per-account so that you can tell the difference between, say, a personal XMPP account and one used for work",
+	"Eion Robb <eionrobb@gmail.com>",
+	"", //URL
+	
+	plugin_load,
+	plugin_unload,
+	NULL,
+	
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	
+	NULL,
+	NULL,
+	NULL,
+	NULL
+};
+
+PURPLE_INIT_PLUGIN(icon_override, plugin_init, info);
